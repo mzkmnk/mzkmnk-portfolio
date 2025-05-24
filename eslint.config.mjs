34f8cefbb6454ac66ef12b9b1e3c4bde29b0f643
mzkmnk <sma0729mex@gmail.com>
2025-05-24 22:53:39 +0900

    feat: add eslint/stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import css from '@eslint/css'
+import { defineConfig } from 'eslint/config'
 import gitignore from 'eslint-config-flat-gitignore'
-
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   gitignore(),
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  stylistic.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/function-call-argument-newline': ['error', 'always'],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-]);
+  { files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'] },
+])

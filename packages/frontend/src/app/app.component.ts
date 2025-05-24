a82c7703655b4ca879c72798140b04935c4b9e68
mzkmnk <sma0729mex@gmail.com>
2025-05-24 23:08:23 +0900

    feat: add tailwind

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  template: `
+    <h1 class="text-5xl">ok</h1>
+  `,
 })
 export class AppComponent {
   title = 'frontend'

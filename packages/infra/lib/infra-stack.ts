82d8d79181d1abec23031a91661cca95ec958c64
mzkmnk <sma0729mex@gmail.com>
2025-05-25 23:33:37 +0900

    chore: 微調整

--- packages/infra/lib/infra-stack.ts
+++ packages/infra/lib/infra-stack.ts
@@ -1,5 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3, aws_cloudfront as cloudfront, aws_cloudfront_origins as cfOrigins,aws_s3_deployment as s3Deployment } from 'aws-cdk-lib';
+import {
+  aws_s3 as s3,
+  aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as cfOrigins,
+  aws_s3_deployment as s3Deployment,
+  aws_certificatemanager as acm
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class InfraStack extends cdk.Stack {
@@ -7,8 +13,8 @@ export class InfraStack extends cdk.Stack {
     super(scope, id, props);
 
     // S3バケットの作成
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-      bucketName: `mzk-mnk-website-bucket`.toLowerCase(),
+    const websiteBucket = new s3.Bucket(this, 'DevWebsiteBucket', {
+      bucketName: `dev-mzk-mnk-website-bucket`.toLowerCase(),
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
       publicReadAccess: false,
@@ -17,9 +23,18 @@ export class InfraStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
+    // 既存のACM証明書を参照
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      'Certificate',
+      'arn:aws:acm:us-east-1:796092240090:certificate/b20d4659-232b-4707-b760-b48965ea86bb',
+    );
+
     // CloudFrontディストリビューションの作成
-    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+    const distribution = new cloudfront.Distribution(this, 'DevWebsiteDistribution', {
       defaultRootObject: 'index.html',
+      domainNames: ['profile.mzkmnk.net'],
+      certificate: certificate,
       defaultBehavior: {
         origin: cfOrigins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -40,7 +55,7 @@ export class InfraStack extends cdk.Stack {
       ],
     });
 
-    new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+    new s3Deployment.BucketDeployment(this, "DevDeployWebsite", {
       sources: [s3Deployment.Source.asset('../frontend/dist/frontend/browser')],
       destinationBucket: websiteBucket,
       distributionPaths: ['/*'],
@@ -49,6 +64,10 @@ export class InfraStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'Hosting URL', {
       value: 'https://' + distribution.distributionDomainName
+    });
+
+    new cdk.CfnOutput(this, 'Custom Domain URL', {
+      value: 'https://mzkmnk.net'
     });
   }
 }

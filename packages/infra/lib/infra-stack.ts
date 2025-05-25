e6aa727f5e9c5f1b934fc58569855f9116304720
mzkmnk <sma0729mex@gmail.com>
2025-05-25 22:32:15 +0900

    feat: s3 + cloudfront

--- packages/infra/lib/infra-stack.ts
+++ packages/infra/lib/infra-stack.ts
@@ -1,16 +1,54 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3, aws_cloudfront as cloudfront, aws_cloudfront_origins as cfOrigins,aws_s3_deployment as s3Deployment } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3バケットの作成
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `mzk-mnk-website-bucket`.toLowerCase(),
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudFrontディストリビューションの作成
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: cfOrigins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+
+    new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3Deployment.Source.asset('../frontend/dist/frontend/browser')],
+      destinationBucket: websiteBucket,
+      distributionPaths: ['/*'],
+      distribution,
+    })
+
+    new cdk.CfnOutput(this, 'Hosting URL', {
+      value: 'https://' + distribution.distributionDomainName
+    });
   }
 }
